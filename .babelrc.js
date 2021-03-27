2b426165f3daeb389f0941bd3b878f26715c03e6
Zé Yuri <jozeyuri@gmail.com>
2021-03-27 20:22:00 -0300

    fix target node version

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  presets : [[
-    "@babel/preset-env", {
-      loose : true,
-      useBuiltIns : "entry",
-      targets : {
-        node : "6.11" // Minimum supported version
-      }
-    }
-  ]],
-  plugins : [
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        useBuiltIns: "entry",
+        targets: {
+          node: "14.16", // Minimum supported version
+        },
+      },
+    ],
+  ],
+  plugins: [
     ["@babel/plugin-proposal-export-default-from", { loose: true }],
     ["@babel/plugin-proposal-export-namespace-from", { loose: true }],
     ["@babel/plugin-proposal-object-rest-spread", { loose: true }],
-    ["add-module-exports", { loose: true }]
-  ]
+    ["add-module-exports", { loose: true }],
+  ],
 };
